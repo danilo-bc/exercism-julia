e9da5d3318d689946570b237b8aa222323c1c942
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-02-24 21:35:50 -0300

    Update on sum_of_squares so it doesn't use 1 allocation

--- difference-of-squares/difference-of-squares.jl
+++ difference-of-squares/difference-of-squares.jl
@@ -2,13 +2,13 @@
 function square_of_sum(n)
     # From calculus/algebra, a fomula for 
     # a triangular number
-    return Int(((1 + n) * ( n/2 ))^2)
+    return ((1 + n) * ( n ))^2 ÷ 4
 end
 
 "Sum the squares of the first `n` positive integers"
 function sum_of_squares(n)
-    # Simply broadcast ^2 to the range and apply sum
-    return Int(sum((1:n).^2))
+    # Using square pyramidal numbers formula (1^2+2^2+...+n^2)
+    return (n* (n+1) * (2*n+1)) ÷ 6
 end
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
