d64c09413aeeba05f79d0a31c18bdfdc59313f02
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-08 14:22:55 -0300

    Refactoring by turning loop into comprehension

--- secret-handshake/secret-handshake.jl
+++ secret-handshake/secret-handshake.jl
@@ -1,18 +1,12 @@
 function secret_handshake(code)
     actions = ["wink", "double blink", "close your eyes", "jump"]
-    bits = digits(code, base=2, pad=5)[1:5]
+    bits = digits(Bool, code, base=2, pad=5)[1:5]
 
-    code_translated = Array{String, 1}(undef,0)
+    code_translated = [actions[i] for i = 1:4 if bits[i]]
 
-    for i = 1:4
-        if bits[i] == 1
-            push!(code_translated, actions[i])
-        end
-    end
-
-    if bits[5] == 0
-        return code_translated
+    if bits[5]
+        return reverse!(code_translated)
     else
-        return code_translated[end:-1:1]
+        return code_translated
     end
 end
