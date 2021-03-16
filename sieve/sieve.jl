5f561e9460396998e66c517417c327d95e11fca0
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-15 23:37:08 -0300

    A concise solution for the sieve problem which finds Prime numbers with ranges!

--- sieve/sieve.jl
+++ sieve/sieve.jl
@@ -1,3 +1,16 @@
 function sieve(limit)
+    test_range = collect(2:limit)
+    test_marks = zeros(Bool,limit-1)
 
+    primes = []
+    for i in test_range
+        if test_marks[i-1] == 0
+            push!(primes, i)
+        end
+        for k in test_range[(i:i:limit).-1]
+            test_marks[k-1] = 1
+        end
+    end
+
+    return primes
 end
