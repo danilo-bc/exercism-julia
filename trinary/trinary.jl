4fb562266cfd684f0da2b2a72aea1a974ab70e09
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-16 00:07:38 -0300

    I'd be happier with the solution if it didn't require me to check special cases. Lines 8 and 11 feel like I actually tinkered someting (but they're probably bad in performance). Anyways, good code brings joy!

--- trinary/trinary.jl
+++ trinary/trinary.jl
@@ -1,3 +1,15 @@
 function trinary_to_decimal(str)
+    for c in str
+        if !isdigit(c)
+            return 0
+        end
+    end
 
+    trits = reverse!([parse(Int,k) for k in [str...]])
+    
+    if unique(trits) ⊆ Set([0,1,2])
+        return sum(trits[i]*3^(i-1) for i = 1:length(str))
+    else
+        return 0
+    end
 end
