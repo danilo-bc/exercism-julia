467002c186085cadc98497070dac366d5127ad29
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-17 17:02:26 -0300

    Using a generator expression to simplify number of lines and idea

--- robot-name/robot-name.jl
+++ robot-name/robot-name.jl
@@ -2,7 +2,7 @@ using Random
 
 mutable struct Robot
     robot_name::String
-    Robot() = new(generate_unique_robot_name())
+    Robot() = new(assign_unique_robot_name())
 end
 
 # Pre-generate all possible names
@@ -10,35 +10,29 @@ const number_of_possible_names = 26^2*10^3
 const robot_name_list = Array{String,1}(undef, number_of_possible_names)
 list_index = 1
 
-# Generate all sequences
-for c1 in 'A':'Z'
-    for c2 in 'A':'Z'
-        for c3 in '0':'9'
-            for c4 in '0':'9'
-                for c5 in '0':'9'
-                    robot_name_list[list_index] = join([c1, c2, c3, c4, c5])
-                    global list_index += 1
-                end
-            end
-        end
-    end
+# Generator of all possible names
+const name_generator = (join([c1, c2, c3, c4, c5]) for c1 = 'A':'Z', c2='A':'Z', c3 = '0':'9', c4 = '0':'9', c5 = '0':'9')
+for name in name_generator
+    robot_name_list[list_index] = name
+    global list_index += 1
 end
-# reset index
+
+# Reset index for use
 list_index = 1
 
-# Pre-generate a random sequence for handing out robot names
-const name_sequence = collect(1:length(robot_name_list))
-shuffle!(name_sequence)
+# Pre-shuffle a random sequence for handing out robot names
+shuffle!(robot_name_list)
 
-function generate_unique_robot_name()
-    list_index > length(robot_name_list) && throw(ErrorException("There are no more unique robot names"))
-    robot_name = robot_name_list[name_sequence[list_index]]
+function assign_unique_robot_name()
+    list_index > length(robot_name_list) && error("There are no more unique robot names")
+
+    robot_name = robot_name_list[list_index]
     global list_index += 1
     return robot_name
 end
 
 function reset!(instance::Robot)
-    instance.robot_name = generate_unique_robot_name()
+    instance.robot_name = assign_unique_robot_name()
 end
 
 function name(instance::Robot)
