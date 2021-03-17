cbf1f7923c069146ce9739dd26358e91251dfc87
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-17 20:06:25 -0300

    The name list is now a vec(tor) and there's no need for a global index. Old names are now reinserted to the pool once a robot is reset

--- robot-name/robot-name.jl
+++ robot-name/robot-name.jl
@@ -7,26 +7,28 @@ end
 
 # Pre-generate all possible names
 const number_of_possible_names = 26^2*10^3
-# const robot_name_list = Array{String,1}(undef, number_of_possible_names)
 
 # Generator of all possible names
-const robot_name_list = [join([c1, c2, c3, c4, c5]) for c1 = 'A':'Z', c2='A':'Z', c3 = '0':'9', c4 = '0':'9', c5 = '0':'9']
+const robot_name_list = vec([join([c1, c2, c3, c4, c5]) for c1 = 'A':'Z', c2='A':'Z', c3 = '0':'9', c4 = '0':'9', c5 = '0':'9'])
 
 # Pre-shuffle a random sequence for handing out robot names
 shuffle!(robot_name_list)
 
-# Number of used names as index
-list_index = 1
 function assign_unique_robot_name()
-    list_index > length(robot_name_list) && error("There are no more unique robot names")
+    isempty(robot_name_list) && error("There are no more unique robot names")
 
-    robot_name = robot_name_list[list_index]
-    global list_index += 1
+    robot_name = pop!(robot_name_list)
+    
     return robot_name
 end
 
 function reset!(instance::Robot)
+    # Save the old name to put it back into the list,
+    # assign a new one and push the old name back
+    old_name = instance.robot_name
     instance.robot_name = assign_unique_robot_name()
+    push!(robot_name_list, old_name)
+    shuffle!(robot_name_list)
 end
 
 function name(instance::Robot)
