1fdcd45f03e9d67e66a1b7267fc2ede6e13ff5bd
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-17 18:26:09 -0300

    robot_name_list generation is much simpler!

--- robot-name/robot-name.jl
+++ robot-name/robot-name.jl
@@ -7,22 +7,16 @@ end
 
 # Pre-generate all possible names
 const number_of_possible_names = 26^2*10^3
-const robot_name_list = Array{String,1}(undef, number_of_possible_names)
-list_index = 1
+# const robot_name_list = Array{String,1}(undef, number_of_possible_names)
 
 # Generator of all possible names
-const name_generator = (join([c1, c2, c3, c4, c5]) for c1 = 'A':'Z', c2='A':'Z', c3 = '0':'9', c4 = '0':'9', c5 = '0':'9')
-for name in name_generator
-    robot_name_list[list_index] = name
-    global list_index += 1
-end
-
-# Reset index for use
-list_index = 1
+const robot_name_list = [join([c1, c2, c3, c4, c5]) for c1 = 'A':'Z', c2='A':'Z', c3 = '0':'9', c4 = '0':'9', c5 = '0':'9']
 
 # Pre-shuffle a random sequence for handing out robot names
 shuffle!(robot_name_list)
 
+# Number of used names as index
+list_index = 1
 function assign_unique_robot_name()
     list_index > length(robot_name_list) && error("There are no more unique robot names")
 
