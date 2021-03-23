c3a6d611cf933c84fd9ed4ab9fd3a8e20535b04b
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-03-23 12:19:58 -0300

    Simple solution to solve the grapheme tests

--- reverse-string/reverse-string.jl
+++ reverse-string/reverse-string.jl
@@ -1,5 +1,9 @@
+using Unicode: graphemes
+
+const TEST_GRAPHEMES = true
+
 """Reverse an input string"""
 function myreverse(str::String)
-    res = Array{Char,1}(str)
+    res = collect(graphemes(str))
     return join(res[end:-1:1])
 end
