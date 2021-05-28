83e33066a7db3878cd31feb98dd16a9e345f750c
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-05-28 17:16:58 -0300

    Initial solution for dnd-character problem

--- dnd-character/dnd-character.jl
+++ dnd-character/dnd-character.jl
@@ -1,11 +1,22 @@
 function modifier(ability)
-
+    return floor((ability - 10)/2)
 end
 
 function ability()
-
+    return rand(3:18, 1)[1]
 end
 
 mutable struct DNDCharacter
-
+    strength    ::Int
+    dexterity   ::Int
+    constitution::Int
+    intelligence::Int
+    wisdom      ::Int
+    charisma    ::Int
+    hitpoints::Int
+    function DNDCharacter()
+        constitution = ability()
+        hitpoints = 10+modifier(constitution) 
+        return new(ability(), ability(), constitution, ability(), ability(), ability(), hitpoints)
+    end
 end
