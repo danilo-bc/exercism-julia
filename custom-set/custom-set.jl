92d22d5e643e3672971bcee36a93bb6dd3c629df
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2021-05-28 22:06:38 -0300

    Temporarily making CustomSet be an AbstractVector type, even if we don't want this in the future, since it provides all the functionality we need. size, IndexStyle and getindex have to be defined as interfaces to provide AbstractVector functionality!

--- custom-set/custom-set.jl
+++ custom-set/custom-set.jl
@@ -1,9 +1,13 @@
-import Base: isempty
+import Base: isempty, size, IndexStyle, getindex
 
-struct CustomSet
-    contents
+struct CustomSet{T} <: AbstractVector{T}
+    contents::Vector{T}
 end
 
 function isempty(cs::CustomSet) 
     return isempty(cs.contents)
 end
+
+size(cs::CustomSet) = size(cs.contents)
+IndexStyle(::Type{<:CustomSet}) = IndexLinear()
+getindex(cs::CustomSet, i::Int) = cs.contents[i]
